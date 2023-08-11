bf26bdc63016e07fc15f4f8b6cff709dae526618
Quackers <shadowrogue624@gmail.com>
2023-08-12 05:38:51 +0800

    Add definition for function fetching user preferences

--- operators.py
+++ operators.py
@@ -83,6 +83,14 @@ class NodeTabSetting(PropertyGroup):
         default="",
     )
 
+def fetch_user_preferences(attr_id=None):
+    prefs = bpy.context.preferences.addons[__package__].preferences
+    if attr_id is None:
+        return prefs
+    else:
+        return getattr(prefs, attr_id)
+    
+
 enum_callback_cache = []
 def other_editors_enum(context):
     items = [(
