e0ef6cc708a7805ec7f7aedeeb8eed7c78d8f712
Quackarooni <83491032+Quackarooni@users.noreply.github.com>
2023-09-20 13:28:57 +0800

    Added support for listing out nodegroup entries for the built-in editors

--- nodelists/__init__.py
+++ nodelists/__init__.py
@@ -5,7 +5,6 @@ from bpy.app.translations import (
     contexts as i18n_contexts,
 )
 
-
 data_list = {
     "CompositorNodeTree" : CompositorNodeTree,
     "GeometryNodeTree" : GeometryNodeTree,
@@ -13,9 +12,51 @@ data_list = {
     "TextureNodeTree" : TextureNodeTree,
 }
 
+
+def fetch_active_nodetree(context):
+    edit_tree = context.space_data.edit_tree
+    node_tree = context.space_data.node_tree
+
+    if edit_tree is not None:
+        return edit_tree
+    else:
+        return node_tree
+
+
+def generate_nodegroup_entries(context):
+    active_tree = fetch_active_nodetree(context)
+    node_groups = context.blend_data.node_groups
+
+    valid_groups = (group for group in node_groups
+        if (group.bl_idname == active_tree.bl_idname and
+            group.name != active_tree.name and
+            not group.contains_tree(active_tree) and
+            not group.name.startswith('.'))
+    )
+
+    # Note - Function for converting strings like 'ShaderNodeTree' to 'ShaderNodeGroup'
+    nodegroup_id = lambda group : group.bl_idname.removesuffix("Tree").__add__("Group")
+
+    # TODO - Implement functionality to set nodegroups, right now it's just for showing up in search
+    group_entries = [generate_entry_item((nodegroup_id(group), {"label":group.name})) for group in valid_groups]
+    return group_entries
+    
+    ''' #Code Snippet for group lookup from old Node Tabber code
+    for tree in node_groups:
+        if tree.type == "GEOMETRY" and tree.name != context.space_data.edit_tree.name:
+            gn_nodeitem = {
+                "idname":"GeometryNodeGroup",
+                "label":tree.name,
+                "settings":{"node_tree": "bpy.data.node_groups['{}']".format(tree.name)},
+            }
+            yield gn_nodeitem
+    '''
+
+
 def add_abbreviation(label):
     abbr = "".join(word[0] for word in label.replace("/", " ").split())
     return f"{label} ({abbr})"
+
 
 def generate_label(*, idname=None, label=None):
     if (label is None) and (idname is None):
@@ -30,6 +71,7 @@ def generate_label(*, idname=None, label=None):
             raise ValueError(f"'{idname}' is not a valid node type.")
             
     return add_abbreviation(iface_(label))
+
 
 def generate_entry_item(item):
     if isinstance(item, tuple):
@@ -59,4 +101,5 @@ def generate_entries(context, editor_type):
         if poll_passed:
             entries.extend([generate_entry_item(item) for item in item_list])
      
+    entries.extend(generate_nodegroup_entries(context))
     return entries
