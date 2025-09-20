a36f1ff4462aab3a72d6c3e077d2fbd3736088fa
Quackers <quackadoobers@gmail.com>
2025-09-20 11:25:00 +0800

    BL Info: Bump up version number

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@ bl_info = {
     "name": "Node Tabber (Development)",
     "author": "Richard Lyons, williamchange, Quackers",
     "blender": (3, 4, 0),
-    "version": (0, 3, 5),
+    "version": (0, 3, 6),
     "description": "Allows quick smart searching of node types.",
     "category": "Node",
 }
